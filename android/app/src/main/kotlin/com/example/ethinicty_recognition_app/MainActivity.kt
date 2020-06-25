abe075c8545a9d4836cb56b3d51e5e2a7631f22e
Karanjot Singh <kjkr7373@gmail.com>
2020-06-25 18:52:52 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/ethinicty_recognition_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ethinicty_recognition_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Ethinicity App'),
+      ),
+    );
+  }
+}
